1f70ebed5f12c234be9e6c46a2906fd75913ae85
dgbailey <bailey.dust@gmail.com>
2019-03-07 14:16:37 -0800

    MVP Components II. Wallls included improper syntax in queryselectorAll and not calling querySelector on the document object

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,32 +3,41 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
-    
+    this.element = element;
     // Get the custom data attribute on the Link
     // this.data;
+    this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab ='${this.data}']`);
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
 
+    this.element.addEventListener('click',() => this.select());
+    
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelectorAll('.tabs-link');
+
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-
+    links.forEach(function(currentValue){
+      currentValue.classList.remove('tabs-link-selected');
+    })
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
+    this.element.classList.add('tabs-link-selected');
     // Call the select method on the item associated with this link
 
+    this.tabItem.select();
   }
 }
 
@@ -36,16 +45,21 @@ class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
     // this.element;
+  this.element = element;
+ 
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
-
+    const items = document.querySelectorAll('.tabs-item');
+    console.log(items);
     // Remove the class "tabs-item-selected" from each element
-    
+    items.forEach(function(currentValue){
+      currentValue.classList.remove('tabs-item-selected');
+    })
     // Add a class named "tabs-item-selected" to this element
     //this.element;
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
@@ -59,4 +73,5 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll('.tabs-link');
+links.forEach(function(currentValue) {return new TabLink(currentValue)});
